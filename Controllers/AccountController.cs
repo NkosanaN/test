bf1931b4c38e67449c205b8587cc5e25c8b10709
Nkosana Ndlela <nkosana@datasaint.com>
2021-07-11 18:35:29 +0200

    text

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MovieApiV.Model;
+using MovieApiV.Services;
+using MovieApiV2Web1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApiV2Web1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        private readonly DataHandler dataHandler;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, 
+            RoleManager<IdentityRole> roleManager, DataHandler handler)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            dataHandler = handler;
+        }
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    //add role here
+                    //await _userManager.AddToRoleAsync(user, "Admin");
+                    return RedirectToAction("RegisterSuccess", "Account");
+                }
+            }
+            ModelState.AddModelError("", "Invalid Register.");
+            //ModelState.AddModelError("Email", "Invalid Register.");
+            return View(model);
+        }
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    var user = await _userManager.FindByNameAsync(model.Email);
+                    //user role list here
+                    var roles = await _userManager.GetRolesAsync(user);
+                    //get default role here
+                    string role = roles.FirstOrDefault();
+                    if (role.Equals("Admin"))
+                    {
+                        return RedirectToAction("Index", "Admin");
+                    }
+                    else if (role.Equals("User"))
+                    {
+                        return RedirectToAction("UserActionName", "UserControllerName");
+                    }
+                    else
+                    {
+                        //do somthing here.put in your logic 
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid ID or Password");
+            return View(model);
+        }
+
+        //must go to loyd's part
+
+        public async Task<ActionResult> BookToDriver()
+        { return View(); }
+        [HttpPost]
+        public async Task<ActionResult> BookToDriver(Bookings model) 
+        {
+            try
+            {
+                var r = await dataHandler.AddBooking(model);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+  
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public ActionResult RegisterSuccess()
+        {
+            return View();
+        }
+        public ActionResult LoginSuccess() 
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/AdminController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MovieApiV.Model;
+using MovieApiV.Services;
+using MovieApiV2Web1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApiV2Web1.Controllers
+{
+    /* Admin steps 
+     * View Test Driving Bookings 
+     * Create Role(s)
+     */
+    //[Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        //private readonly DataHandler dataHandler;
+        private readonly DataHandler dataHandler;
+        public AdminController(RoleManager<IdentityRole> roleManager, DataHandler handler) 
+        {
+            _roleManager = roleManager;
+            dataHandler = handler;
+        }
+        // GET: AdminController
+        public async Task<ActionResult> Index()
+        {
+            var model = await dataHandler.BookingListGet();
+            return View(model);
+        }
+
+        // GET: AdminController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Role role)
+        {
+            try
+            {
+                ModelState.AddModelError("RoleName", "The Role already exist.");
+                var roleExist = await _roleManager.RoleExistsAsync(role.RoleName);
+                if (!roleExist) 
+                {
+                    var r = await _roleManager.CreateAsync(new IdentityRole(role.RoleName));
+                }
+            }
+            catch
+            {
+            }
+
+            return View(role);
+        }
+
+        // GET: AdminController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            var model = await dataHandler.BookingGetSingle(id);
+            return View(model);
+        }
+
+        // POST: AdminController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, Bookings model)
+        {
+            try
+            {
+                var r = await dataHandler.UpdateBooking(model, id);
+                if (r)
+                    return RedirectToAction(nameof(Index));
+                else 
+                    View("Error");
+
+            }
+            catch (Exception ex)
+            {
+                return View(ex);
+            }
+            return View(model);
+        }
+
+        // GET: AdminController/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            var model = await dataHandler.BookingGetSingle(id);
+            return View(model);
+        }
+
+        // POST: AdminController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id,Bookings model)
+        {
+            try
+            {
+                var r = await dataHandler.DeleteBooking(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+    }
+}
